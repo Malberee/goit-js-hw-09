d9f35a89b4de119e0669b020b5a9dcebc9472722
Malberee <82259650+Malberee@users.noreply.github.com>
2023-03-13 20:52:59 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.disabled = true;
+let timerId = null;
+
+startBtn.addEventListener('click', e => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', e => {
+  stopBtn.disabled = true;
+    startBtn.disabled = false;
+    
+    clearInterval(timerId)
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
