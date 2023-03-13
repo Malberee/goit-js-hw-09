dd634bf2ee56521d11b31963d97aeaa4102b4ca1
Malberee <82259650+Malberee@users.noreply.github.com>
2023-03-13 18:53:29 +0000

    Deploying to gh-pages from @ Malberee/goit-js-hw-09@d9f35a89b4de119e0669b020b5a9dcebc9472722 🚀

---
+++ 01-color-switcher.848b9777.js
@@ -0,0 +1,2 @@
+const t=document.body,e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0;let a=null;e.addEventListener("click",(l=>{e.disabled=!0,d.disabled=!1,a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(t=>{d.disabled=!0,e.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.848b9777.js.map
